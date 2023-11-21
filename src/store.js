dc75f9752a9ddd70706ef782dcb613b263b7d7fb
Cookie <eg.melnikk2@gmail.com>
2023-11-21 22:14:36 +0300

    third task

--- src/store.js
+++ src/store.js
@@ -71,6 +71,9 @@ class Store {
       list: this.state.list.map(item => {
         if (item.code === code) {
           item.selected = !item.selected;
+		  if (item.selected) {
+			item.selectedTimes ? item.selectedTimes++ : item.selectedTimes = 1;
+		  }
         } else {
 			item.selected = false;
 		}
